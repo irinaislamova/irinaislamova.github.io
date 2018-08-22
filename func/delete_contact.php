718f5080b9211b13680a9dc767d46df11a58b7b8
irinaislamova <albina-ole@mail.ru>
2018-08-22 16:40:50 +0500

    Add files via upload

--- func/delete_contact.php
+++ func/delete_contact.php
@@ -1,13 +1,11 @@
 <?php
 header("Content-Type: text/html; charset=utf-8");
 
-if ($_GET['id'] != 0) {
-    require_once '../database.php';
-    $id = $_GET['id'];
-    deletePeople($id);
-    header("Location: /index.php");
-}
+
 require_once '../database.php';
-    insertPeople($_POST['name'], $_POST['phone'], $_POST['adress']);
-    header("Location: /index.php");  
+if ($_GET['id'] != null) {
+	deletePeople($_GET['id']);
+}
+header("Location: /index.php");
+
 ?>
