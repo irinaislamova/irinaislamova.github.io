ed5b4911e28beef24cd2912da4d8ceec09c1c432
irinaislamova <albina-ole@mail.ru>
2018-08-23 21:22:51 +0500

    Update edit_contact.php

--- func/edit_contact.php
+++ func/edit_contact.php
@@ -1,7 +1,6 @@
 <?php
 header("Content-Type: text/html; charset=utf-8");
 error_reporting(0);
-error_reporting(E_ERROR|E_WARNING|E_PARSE|E_NOTICE);
 require_once '../database.php';
 $_POST['phone'] = (int)($_POST['phone']);
 
